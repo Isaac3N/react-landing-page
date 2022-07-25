d051832e615f40670891b48517357a3e20b74709
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-25 14:26:07 +0100

    updates, consistent background

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Article from '../../components/article/Article'
 import {blog01, blog02, blog03, blog04, blog05 } from "./imports"
+import "./blog.css"
 
 const Blog = () => {
   return (
@@ -11,10 +12,10 @@ const Blog = () => {
         </h1>
       </div>
       <div className="gpt3__blog-container">
-        <div className="pt3__blog-container_groupA">
+        <div className="gpt3__blog-container_groupA">
           <Article imgUrl={blog01} date="Sep 19 2022" title=" GPT-3 and Open AI is the future. Let us explore how it is built!"/>
         </div>
-        <div className="pt3__blog-container_groupB">
+        <div className="gpt3__blog-container_groupB">
           <Article imgUrl={blog02} date="Sep 19 2022" title=" GPT-3 and Open AI is the future. Let us explore how it is built!"/> 
           <Article imgUrl={blog03} date="Sep 19 2022" title=" GPT-3 and Open AI is the future. Let us explore how it is built!"/> 
           <Article imgUrl={blog04} date="Sep 19 2022" title=" GPT-3 and Open AI is the future. Let us explore how it is built!"/>
